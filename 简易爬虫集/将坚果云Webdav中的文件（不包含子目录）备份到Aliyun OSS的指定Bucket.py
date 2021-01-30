a83e2a1f9c3d9656bacfa7592ba5578826716068
yindaheng98 <yindaheng98@163.com>
2021-01-30 20:50:00 +0800

    bug fix

--- 简易爬虫集/将坚果云Webdav中的文件（不包含子目录）备份到Aliyun OSS的指定Bucket.py
+++ 简易爬虫集/将坚果云Webdav中的文件（不包含子目录）备份到Aliyun OSS的指定Bucket.py
@@ -13,7 +13,7 @@ now_str = datetime.now().strftime(dtf)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
-ch.setLevel(logging.WARNING)
+ch.setLevel(logging.INFO)
 formatter = logging.Formatter(
     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
