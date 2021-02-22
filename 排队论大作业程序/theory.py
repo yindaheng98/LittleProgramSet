5787c41a0824c0fa24969f9eaa1c1fcfc72ed1ab
yindaheng98 <yindaheng98@163.com>
2021-02-22 19:47:39 +0800

    理论值完成

---
+++ 排队论大作业程序/theory.py
@@ -0,0 +1,11 @@
+import math
+
+
+def MMn(n, _lambda, _miu):
+    rho = _lambda / _miu
+    P0 = 1 / (
+            sum([rho ** i / math.factorial(i) for i in range(0, n + 1)]) +
+            rho ** (n + 1) / (math.factorial(n) * (n - rho))
+    )
+    Elq = (n * rho ** (n + 1) * P0) / (n * math.factorial(n) * (1 - rho / n) ** 2)
+    return Elq
