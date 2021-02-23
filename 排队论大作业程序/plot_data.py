428c9ff74f20e6d4d079db337af45bc15b0a935f
yindaheng98 <yindaheng98@163.com>
2021-02-23 16:55:13 +0800

    画图程序安排好了，就差计算了

--- 排队论大作业程序/plot_data.py
+++ 排队论大作业程序/plot_data.py
@@ -1,6 +1,7 @@
 import json
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 plt.rcParams['font.sans-serif'] = ['SimHei']
 N = 300
@@ -14,27 +15,27 @@ n_list = result['n_list']
 result = result['result']
 
 names = {
-    'MMn': 'M|M|n 队列',
-    'nMM1': '概率分裂的 n 个 M|M|1 队列',
-    'nEnM1': 'n 个 En|M|n 队列',
-    'nMM1Short': '顾客选择最短队列的 n 个 M|M|1 队列',
+    'MMn': '$M|M|n$队列',
+    'nMM1': '概率分裂的$n$个$M|M|1$队列',
+    'nEnM1': '$n$个$En|M|n$队列',
+    'nMM1Short': '顾客选择最短队列的$n$个$M|M|1$队列',
     'simulate': '仿真值',
     'theory': '理论值'
 }
 
 colors = {
-    'MMn': 'cornflowerblue',
-    'nMM1': 'chocolate',
-    'nEnM1': 'forestgreen',
-    'nMM1Short': 'hotpink',
+    'MMn': list(mcolors.TABLEAU_COLORS)[0],
+    'nMM1': list(mcolors.TABLEAU_COLORS)[1],
+    'nEnM1': list(mcolors.TABLEAU_COLORS)[2],
+    'nMM1Short': list(mcolors.TABLEAU_COLORS)[3],
 }
 
 x = rho_list
 for n in result:
     data = result[n]
-    fname = 'figure/n=%s.png' % n
+    fname = 'figure/n=%s的所有队列.png' % n
     print(fname)
-    plt.figure()
+    plt.figure(figsize=(13, 12))
     for model in data:
         label_pre = names[model]
         for mod in data[model]:
@@ -46,7 +47,28 @@ for n in result:
                 plt.plot(x, y, label=label, color=colors[model], linestyle='--')
             else:
                 plt.plot(x, y, label=label, color=colors[model])
-    plt.xlabel(r'$\frac{\lambda}{n\mu}$')
-    plt.ylabel(r'$Elq$')
-    plt.legend()
+    plt.xlabel(r'$\frac{\lambda}{n\mu}$', fontsize=30)
+    plt.ylabel(r'$Elq$', fontsize=30)
+    plt.legend(fontsize=15)
+    plt.savefig(fname)
+
+colors_n = {str(i + 1): c for i, c in enumerate(list(mcolors.TABLEAU_COLORS))}
+for model in colors:
+    fname = 'figure/%s.png' % names[model].replace('$', "").replace('|', "")
+    print(fname)
+    plt.figure(figsize=(13, 12))
+    for n in result:
+        label_pre = '$n=$%2s' % n
+        for mod in result[n][model]:
+            label_suf = names[mod]
+            label = label_pre + ' - ' + label_suf
+            y = result[n][model][mod]
+            print(label, y)
+            if mod == 'theory':
+                plt.plot(x, y, label=label, color=colors_n[n], linestyle='--')
+            else:
+                plt.plot(x, y, label=label, color=colors_n[n])
+    plt.xlabel(r'$\frac{\lambda}{n\mu}$', fontsize=30)
+    plt.ylabel(r'$Elq$', fontsize=30)
+    plt.legend(fontsize=15)
     plt.savefig(fname)
