f90be85279d5e5d58f0a6f84ae80affc9d91f0f9
yindaheng98 <yindaheng98@163.com>
2021-02-23 10:59:09 +0800

    用class组织模拟器

--- 排队论大作业程序/main.py
+++ 排队论大作业程序/main.py
@@ -1,30 +1,32 @@
+import random
+
 from EventLoop import EventLoop
 from Q import Q
-import random
-from theory import MMn
-import matplotlib.pyplot as plt
-import numpy as np
-
-'''初始化配置'''
-N = 30000
-_lambda = 3.
-_miu = 5.
-n = 3
-
-'''运行时配置'''
-event_loop = EventLoop()
-q = Q(event_loop, _miu, n)
 
 
-def ArriveCallback(_lambda):
-    global N
-    N -= 1
-    arrive_interval = random.expovariate(_lambda)  # 随机生成到达时间间隔
-    event_loop.setTimeout(arrive_interval, lambda: ArriveCallback(_lambda))  # 一定时间后再次到达
-    q.arrive()
+class MMnSimulator:
+    def __init__(self, _lambda, _miu, n):
+        self._lambda = _lambda
+        self._miu = _miu
+        self.n = n
+        self.event_loop = None
+        self.q = None
+        self.N = 0
+
+    def start(self, N):
+        self.event_loop = EventLoop()
+        self.q = Q(self.event_loop, self._miu, self.n)
+        self.N = N
+        self.event_loop.setTimeout(0, lambda: self.__arrive_callback())
+        self.event_loop.loopUntil(lambda: self.N <= 0)
+
+    def __arrive_callback(self):
+        self.N -= 1
+        arrive_interval = random.expovariate(self._lambda)  # 随机生成到达时间间隔
+        self.event_loop.setTimeout(arrive_interval, lambda: self.__arrive_callback())  # 一定时间后再次到达
+        self.q.arrive()
 
 
-event_loop.setTimeout(0, lambda: ArriveCallback(_lambda))
-event_loop.loopUntil(lambda: N <= 0)
-print("Elq=%f" % q.Elq())
-print("Elq=%f" % MMn(n, _lambda, _miu))
+MMn = MMnSimulator(_lambda, _miu, n)
+MMn.start(30000)
+print("Elq=%f" % MMn.q.Elq())
