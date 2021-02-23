b76b04c06dd288019010959dae02b15bf0bc363c
yindaheng98 <yindaheng98@163.com>
2021-02-23 12:03:04 +0800

    又搞定一个

--- 排队论大作业程序/main.py
+++ 排队论大作业程序/main.py
@@ -1,5 +1,9 @@
 from MMnSimulator import MMnSimulator
+from nMM1Simulator import nMM1Simulator
 
 MMn = MMnSimulator(3., 5., 3)
+nMM1 = nMM1Simulator(3., 5., 3)
 print("Elq=%f" % MMn.run(30000))
 print("Elq=%f" % MMn.theory())
+print("Elq=%f" % nMM1.run(30000))
+print("Elq=%f" % nMM1.theory())
---
+++ 排队论大作业程序/nMM1Simulator.py
@@ -0,0 +1,36 @@
+import random
+
+from EventLoop import EventLoop
+from Q import Q
+
+
+class nMM1Simulator:
+    def __init__(self, _lambda, _miu, n):
+        self._lambda = float(_lambda)
+        self._miu = float(_miu)
+        self.n = int(n)
+        self.event_loop = None
+        self.q = []
+        self.N = 0
+
+    def run(self, N):
+        self.event_loop = EventLoop()
+        self.q = []
+        for _ in range(0, self.n):
+            self.q.append(Q(self.event_loop, self._miu, 1))
+        self.N = N
+        self.event_loop.setTimeout(0, lambda: self.__arrive_callback())
+        self.event_loop.loopUntil(lambda: self.N <= 0)
+        return sum([q.Elq() for q in self.q])
+
+    def __arrive_callback(self):
+        self.N -= 1
+        arrive_interval = random.expovariate(self._lambda)  # 随机生成到达时间间隔
+        self.event_loop.setTimeout(arrive_interval, lambda: self.__arrive_callback())  # 一定时间后再次到达
+        self.q[random.randint(0, self.n - 1)].arrive()
+
+    def theory(self):
+        _lambda, _miu, n = self._lambda, self._miu, self.n
+        rho = _lambda / _miu
+        Elq = (rho * _lambda) / (n * _miu - _lambda)
+        return Elq
