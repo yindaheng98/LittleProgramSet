81428d59df5c1ffcea80c1cd04e131631c5debff
yindaheng98 <yindaheng98@163.com>
2022-12-25 23:42:37 +0800

    改成python

---
+++ 简易爬虫集/监视Zotero数据库文件修改并自动上传.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from datetime import datetime
+import subprocess
+
+PathToScript = os.path.dirname(sys.argv[0])
+PathToSqlite = os.path.join(PathToScript, "zotero.sqlite")
+PathToRecord = os.path.join(PathToScript, "zotero.sqlite.LastWriteTime.txt")
+RecordLastWriteTime = os.path.getmtime(PathToRecord)
+LastWriteTime = os.path.getmtime(PathToSqlite)
+if RecordLastWriteTime == LastWriteTime:
+    exit(0)
+subprocess.run([
+    os.path.join(PathToScript, "rclone.exe"),
+    "--log-level", "DEBUG",
+    "copy",
+    PathToSqlite,
+    "jianguoyun:zotero"
+])
+with open(PathToRecord, "w", encoding='utf8') as f:
+    f.write(str(datetime.fromtimestamp(LastWriteTime)))
+os.utime(PathToRecord, (LastWriteTime, LastWriteTime))
