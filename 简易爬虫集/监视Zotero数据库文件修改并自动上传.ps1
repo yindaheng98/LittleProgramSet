d601d30b51f63617196ce0c97f19d3617937ffee
yindaheng98 <yindaheng98@163.com>
2022-12-25 23:19:05 +0800

    ok了

--- 简易爬虫集/监视Zotero数据库文件修改并自动上传.ps1
+++ 简易爬虫集/监视Zotero数据库文件修改并自动上传.ps1
@@ -1,9 +1,10 @@
-$PathToMonitor = "D:\Documents\Zotero\zotero.sqlite"
+$PathToSqlite = "D:\Documents\Zotero\zotero.sqlite"
 $PathToRecord = "D:\Documents\Zotero\zotero.sqlite.LastWriteTime.txt"
-$RecordLastWriteTime = (Get-Content $PathToRecord)
-$LastWriteTime = (Get-ChildItem $PathToMonitor).LastWriteTime
+$RecordLastWriteTime = (Get-ChildItem $PathToRecord).LastWriteTime
+$LastWriteTime = (Get-ChildItem $PathToSqlite).LastWriteTime
 if ($RecordLastWriteTime -eq $LastWriteTime) {
     exit 0
 }
-
-(Write-Output $LastWriteTime) > $PathToRecord
+& "D:\Documents\MyPrograms\rclone-v1.61.1-windows-amd64\rclone.exe" --log-level DEBUG copy $PathToSqlite jianguoyun:zotero
+Write-Output $LastWriteTime > $PathToRecord
+(Get-ChildItem $PathToRecord).LastWriteTime = $LastWriteTime
