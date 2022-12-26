7e84e3b069cad8e48ce2744cb87cfb606e087e72
yindaheng98 <yindaheng98@163.com>
2022-12-26 16:41:14 +0800

    no need ls

--- 简易爬虫集/Zotero数据备份到Aliyun OSS/将坚果云中的Zotero附件备份到OSS.sh
+++ 简易爬虫集/Zotero数据备份到Aliyun OSS/将坚果云中的Zotero附件备份到OSS.sh
@@ -3,12 +3,8 @@ ROOT=$(dirname ${BASH_SOURCE[0]})
 source $ROOT/init.sh
 $EXEC copy jianguoyun:zotero/zotero.sqlite $ROOT
 for i in $(echo "select key from items;" | sqlite3 $ROOT/zotero.sqlite); do
-    $EXEC ls jianguoyun:zotero/$i.zip
-    if [$? -eq 0]; then
-        $EXEC copy jianguoyun:zotero/$i.zip oss:zotero-backup/attachments
-    fi
-    $EXEC ls jianguoyun:zotero/$i.prop
-    if [$? -eq 0]; then
-        $EXEC copy jianguoyun:zotero/$i.prop oss:zotero-backup/attachments
-    fi
+    set +e
+    $EXEC copy jianguoyun:zotero/$i.zip  oss:zotero-backup/attachments
+    $EXEC copy jianguoyun:zotero/$i.prop oss:zotero-backup/attachments
+    set -e
 done
