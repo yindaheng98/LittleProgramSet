f63a131fc9881b1522ee9e762fcc4f6f0b00257c
yindaheng98 <yindaheng98@163.com>
2022-12-27 22:48:19 +0800

    先比大小后比时间

--- 简易爬虫集/监视Zotero数据库文件修改并自动上传/监视Zotero数据库文件修改并自动上传.py
+++ 简易爬虫集/监视Zotero数据库文件修改并自动上传/监视Zotero数据库文件修改并自动上传.py
@@ -7,13 +7,33 @@ from config import config
 PathToScript = os.path.dirname(sys.argv[0])
 PathToSqlite = os.path.join(PathToScript, "zotero.sqlite")
 PathToRecord = os.path.join(PathToScript, "zotero.sqlite.LastWriteTime.txt")
+
+
+try:
+    with open(PathToRecord, "r", encoding='utf8') as f:
+        RecordSize = int(f.read())
+except Exception as e:
+    print(e)
+    RecordSize = 0
+Size = os.path.getsize(PathToSqlite)
+if RecordSize == Size:
+    print(RecordSize, "==", Size)
+    exit(0)
+print(RecordSize, "!=", Size)
+
 RecordLastWriteTime = os.path.getmtime(PathToRecord) if os.path.exists(PathToRecord) else 0
 LastWriteTime = os.path.getmtime(PathToSqlite)
 if RecordLastWriteTime == LastWriteTime:
     print(RecordLastWriteTime, "==", LastWriteTime)
     exit(0)
-client = Client(**config)
+print(RecordLastWriteTime, "!=", LastWriteTime)
+
+client = Client(**config, timeout=300, trust_env=False)
 client.upload_file(PathToSqlite, "zotero/zotero.sqlite", overwrite=True)
+
+print("RecordSize", Size)
 with open(PathToRecord, "w", encoding='utf8') as f:
-    f.write(str(datetime.fromtimestamp(LastWriteTime)))
+    f.write(str(Size))
+
+print("LastWriteTime", LastWriteTime)
 os.utime(PathToRecord, (LastWriteTime, LastWriteTime))
